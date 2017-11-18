301a00e7b4dc6f0d6c8c1c12e8409a9e2019d557
Olusola Oseni <kenolusola@gmail.com>
2017-11-18 18:05:14 +0100

    Refactor config.js and index.js to es6

---
+++ server/config/config.js
@@ -0,0 +1,24 @@
+const environment = process.env.NODE_ENV || 'development';
+// const environment = environmentVariables.NODE_ENV || 'development';
+const dialect = 'postgres';
+const url = process.env.DATABASE_URL_DEV;
+// const url = environmentVariables.DATABASE_URL_DEV;
+
+const devMode = environment === ('development' || 'test');
+
+const config = {
+  url,
+  dialect,
+  logging: devMode ? log => log : false,
+  dialectOptions: {
+    multipleStatements: true
+  }
+};
+
+if (!devMode) {
+  config.ssl = true;
+  config.dialectOptions.ssl = {
+    require: !devMode
+  };
+}
+module.exports = config;

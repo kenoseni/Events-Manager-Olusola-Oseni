fbfbbbd5e56a463810a34b08514de01e1f2d7674
Olusola Oseni <kenolusola@gmail.com>
2017-11-18 15:48:38 +0100

    Create express application in app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import volleyball from 'volleyball';
+import bodyParser from 'body-parser';
+// Set up the express app
+const app = express();
+
+// Log requests to the console.
+app.use(volleyball);
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set the app entry port
+app.set('port', 8000);
+
+app.listen(app.get('port'));
+
+// Setup a default catch-all route that sends back a welcome message.
+app.get('*', (req, res) => res.status(200).send({
+    message: 'Welcome to my API.'
+}));
+export default app;

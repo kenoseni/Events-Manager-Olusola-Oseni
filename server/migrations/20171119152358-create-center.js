6db9c7689819e3b9058490f09d33f5588309081b
Olusola Oseni <kenolusola@gmail.com>
2017-11-19 16:45:30 +0100

    refactor center migration file to es6

---
+++ server/migrations/20171119152358-create-center.js
@@ -0,0 +1,34 @@
+export default {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Centers', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    name: {
+      type: Sequelize.STRING
+    },
+    description: {
+      type: Sequelize.TEXT
+    },
+    location: {
+      type: Sequelize.STRING
+    },
+    address: {
+      type: Sequelize.STRING
+    },
+    capacity: {
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Centers')
+};

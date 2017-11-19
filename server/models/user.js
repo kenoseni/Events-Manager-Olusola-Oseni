9c678cba344b0d61b24dbdbffb78a45c4f48b22b
Olusola Oseni <kenolusola@gmail.com>
2017-11-19 15:20:10 +0100

    Create user model and refactor code to es6

---
+++ server/models/user.js
@@ -0,0 +1,72 @@
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Firstname field cannot be empty'
+        },
+        isAlphanumeric: {
+          args: true,
+          msg: 'Allows only alphanumeric characters'
+        }
+      },
+      set(value) {
+        this.setDataValue('firstname', value.toString().toLowerCase().trim());
+      }
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Lastname field cannot be empty'
+        },
+        isAlphanumeric: {
+          args: true,
+          msg: 'Allows only alphanumeric characters'
+        }
+      },
+      set(value) {
+        this.setDataValue('lastname', value.toString().toLowerCase().trim());
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Email field cannot be empty'
+        },
+        isEmail: {
+          args: true,
+          msg: 'Please supply the right email format'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Password field cannot be empty'
+        },
+        len: {
+          args: [8],
+          msg: 'Password must contain a minimum of 8 characters'
+        }
+      },
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+  return User;
+};

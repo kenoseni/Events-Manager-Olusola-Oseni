327840b03a93b61e1fefa8c22801e92667656f3a
Olusola Oseni <kenolusola@gmail.com>
2017-11-19 07:37:12 +0100

    Fix eslint indent error in config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
+
 const environment = process.env.NODE_ENV || 'development';
 const dialect = 'postgres';
 const url = process.env.DATABASE_URL_DEV;
-// const url = environmentVariables.DATABASE_URL_DEV;
 
 const devMode = environment === ('development' || 'test');
 

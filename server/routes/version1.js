92d1c0aca7278283c256b35d4cb43cc8aeb32e7c
Olusola Oseni <kenolusola@gmail.com>
2017-11-20 09:20:31 +0100

    Add route handler for the /users/login endpoint

--- server/routes/version1.js
+++ server/routes/version1.js
@@ -4,4 +4,6 @@ import controller from '../controller';
 const router = express.Router();
 // Register a user on the platform
 router.post('/users', controller.userController.signup);
+// Signin a user on the platform
+router.post('/users/login', controller.userController.login);
 export default router;

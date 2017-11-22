80d33c417000d2ea02e19702edaf9bf9f524e44e
Olusola Oseni <kenolusola@gmail.com>
2017-11-22 14:05:03 +0100

    Fix line breaks issues

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,9 +1,11 @@
 import tokenController from './authenticate';
 import adminValidation from './adminValidation';
 import userValidation from './userValidation';
+import centerValidation from './centerValidation';
 
 export default {
   tokenController,
   adminValidation,
-  userValidation
+  userValidation,
+  centerValidation
 };

d1cac1cc4079880efdf1bfc3da155d59d0d36012
Olusola Oseni <kenolusola@gmail.com>
2017-11-22 17:26:39 +0100

    Export event input validation

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -2,10 +2,12 @@ import tokenController from './authenticate';
 import adminValidation from './adminValidation';
 import userValidation from './userValidation';
 import centerValidation from './centerValidation';
+import eventValidation from './eventValidation';
 
 export default {
   tokenController,
   adminValidation,
   userValidation,
-  centerValidation
+  centerValidation,
+  eventValidation
 };

87c4da042734df337200467e4ca49e8ff4a84772
Olusola Oseni <kenolusola@gmail.com>
2017-11-24 12:37:53 +0100

    Modified .env and .config files

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const environment = process.env.NODE_ENV || 'development';
 const dialect = 'postgres';
-const url = process.env.DATABASE_URL_DEV;
+const url = process.env.DATABASE_URL;
 
 const devMode = environment === ('development' || 'test');
 

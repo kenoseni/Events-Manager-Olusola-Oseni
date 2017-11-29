36091d71b5643f872385edc699a4cea49c3e3f12
Olusola Oseni <kenolusola@gmail.com>
2017-11-29 13:34:55 +0100

    Removed siginup and signin component

--- client/components/App.js
+++ client/components/App.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
-import Navbar from './Navbar'
-import LandingPage from './LandingPage'
-import SignUp from './SignUp'
-import SignIn from './SignIn'
+import Navbar from './Navbar';
+import LandingPage from './LandingPage';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import Main from './Main';
+import SigninPage from './SigninPage'
 
 export default class App extends Component {
     render () {
+        
         return (
         <div>
             <Navbar />
-            <LandingPage />
-            <SignUp />
-            <SignIn />   
+            <LandingPage /> 
+            <Main /> 
+            <SigninPage /> 
         </div>
         )
     }

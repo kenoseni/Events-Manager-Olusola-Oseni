bbadf8bab6161db54c05901a5a4df114baadfda2
Olusola Oseni <kenolusola@gmail.com>
2017-12-19 11:49:10 +0100

    Modified create admin

--- server/controller/user.js
+++ server/controller/user.js
@@ -28,11 +28,12 @@ class userController {
       .then((user) => {
         const token = auth.tokenController.createToken(user);
         return res.status(201).json({
-          status: 'Success',
-          message: 'User successfully signed up',
-          firstname: user.firstname,
-          lastname: user.lastname,
           data: {
+            status: 'Success',
+            message: 'User successfully signed up',
+            firstname: user.firstname,
+            lastname: user.lastname,
+            role: user.role,
             token
           }
         });
@@ -60,9 +61,11 @@ class userController {
       })
       .then((user) => {
         if (!user) {
-          return res.status(401).json({
-            status: 'Fail',
-            message: 'Email or password incorrect'
+          return res.status(400).json({
+            data: {
+              status: 'Fail',
+              message: 'Email not found'
+            }
           });
         }
         const encrypted = user.password;
@@ -70,23 +73,27 @@ class userController {
           .then((correct) => {
             if (!correct) {
               res.status(401).json({
-                status: 'fail',
-                message: 'Email or password incorrect'
+                data: {
+                  status: 'Fail',
+                  message: 'Email or password incorrect'
+                }
               });
             }
             const token = auth.tokenController.createToken(user);
             return res.status(200).json({
-              status: 'Success',
-              message: 'User logged in',
               data: {
+                status: 'Success',
+                message: 'User logged in',
                 token
               }
             });
           });
       })
       .catch(error => res.status(500).json({
-        status: 'Error',
-        message: error.message
+        data: {
+          status: 'Error',
+          message: error.message
+        }
       }));
   }
   /**
@@ -100,22 +107,44 @@ class userController {
      */
   static createAdmin(req, res) {
     return db.User
-      .find({
-        where: {
-          id: req.decoded.userid
-        },
+      .findById(req.params.userId)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ error: 'User not found' });
+        }
+        if (req.decoded.userrole !== 'superadmin') {
+          return res.status(401).json({
+            data: {
+              status: 'Fail',
+              message: 'You are not authorised to modify this user'
+            }
+          });
+        }
+        if (req.decoded.userid === req.params.userId) {
+          return res.status(403).json({
+            data: {
+              status: 'Fail',
+              message: 'Super Admin cannot be modified'
+            }
+          });
+        }
+        user
+          .update({
+            isAdmin: true,
+            role: 'admin'
+          });
       })
-      .then(user => user
-        .update({
-          isAdmin: true
-        }))
       .then(() => res.status(200).json({
-        status: 'Success',
-        message: 'Admin created successfully'
+        data: {
+          status: 'Success',
+          message: 'User successfully upgraded to admin'
+        }
       }))
       .catch(error => res.status(500).json({
-        status: 'Error',
-        message: error.message
+        data: {
+          status: 'Error',
+          message: error.message
+        }
       }));
   }
 }

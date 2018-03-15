cdcee86c1980139d3da358f010eeacb0f86d0540
Olusola Oseni <kenolusola@gmail.com>
2018-03-16 00:42:53 +0100

    Modified store to use redux devtools extention

--- client/store.js
+++ client/store.js
@@ -1,9 +1,13 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+// importing the root reducer
 import reducer from './reducers';
 
 const middleware = applyMiddleware(thunk, logger);
 
-export default createStore(reducer, middleware);
+const store = createStore(reducer, composeWithDevTools(middleware));
+
+export default store;

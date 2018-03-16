c4126bbb92307ca386301e87672dd280ffe70c80
Olusola Oseni <kenolusola@gmail.com>
2018-03-16 22:35:22 +0100

    Add login action

--- client/actions/UserActions.js
+++ client/actions/UserActions.js
@@ -8,14 +8,26 @@ const createUser = userDetails => (dispatch) => {
       const { token } = res.data.data;
       localStorage.setItem('x-access-token', token);
       dispatch({ type: 'SIGNUP_USER_RESOLVED', payload: res.data });
-      // history.push('/events');
-      // this.context.router.push('/events');
     })
     .catch((err) => {
       dispatch({ type: 'SIGNUP_USER_REJECTED', payload: err.response.data });
-      // history.push('/');
+    });
+};
+
+// User Login action
+const userLogin = loginDetails => (dispatch) => {
+  dispatch({ type: 'LOGIN_USER' });
+  return axios.post('/api/v1/users/login', loginDetails)
+    .then((res) => {
+      const { token } = res.data.data;
+      localStorage.setItem('x-access-token', token);
+      dispatch({ type: 'LOGIN_RESOLVED', payload: res.data });
+    })
+    .catch((err) => {
+      dispatch({ type: 'LOGIN_REJECTED', payload: err.response.data });
     });
 };
 export {
   createUser,
+  userLogin
 };

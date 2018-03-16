eddd2bc2846391292e2649873f3fc4f41d1f36ae
Olusola Oseni <kenolusola@gmail.com>
2018-03-16 16:26:15 +0100

    Refactored create user action

--- client/actions/UserActions.js
+++ client/actions/UserActions.js
@@ -1,45 +1,21 @@
 import axios from 'axios';
 
-const createUser = (userDetails) => {
-  return (dispatch) => {
-    dispatch({ type: 'FETCH_USER' });
-    axios.post('http://localhost:8000/api/v1/users', userDetails)
-      .then((res) => {
-        dispatch({ type: 'FETCH_USER_RESOLVED', payload: res.data });
-      })
-      .catch((err) => {
-        dispatch({ type: 'FETCH_USER_REJECTED', payload: err.response.data });
-      });
-  };
+// Create User
+const createUser = userDetails => (dispatch) => {
+  dispatch({ type: 'SIGNUP_USER' });
+  return axios.post('/api/v1/users', userDetails)
+    .then((res) => {
+      const { token } = res.data.data;
+      localStorage.setItem('x-access-token', token);
+      dispatch({ type: 'SIGNUP_USER_RESOLVED', payload: res.data });
+      // history.push('/events');
+      // this.context.router.push('/events');
+    })
+    .catch((err) => {
+      dispatch({ type: 'SIGNUP_USER_REJECTED', payload: err.response.data });
+      // history.push('/');
+    });
 };
-
-const userLogin = (loginDetails) => {
-  return (dispatch) => {
-    dispatch({ type: 'LOGIN_USER' });
-    axios.post('http://localhost:8000/api/v1/users/login', loginDetails)
-      .then((res) => {
-        dispatch({ type: 'LOGIN_RESOLVED', payload: res.data });
-      })
-      .catch((err) => {
-        dispatch({ type: 'LOGIN_REJECTED', payload: err.response.data });
-      });
-  };
-};
-const getCenters = (allCenters) => {
-  return (dispatch) => {
-    // dispatch({ type: 'ALL_CENTERS' });
-    axios.get('http://localhost:8000/api/v1/centers', allCenters)
-      .then((res) => {
-        dispatch({ type: 'ALL_CENTERS_RESOLVED', payload: res.data });
-      })
-      .catch((err) => {
-        dispatch({ type: 'ALL_CENTERS_REJECTED', payload: err.response.data });
-      });
-  };
-};
-
 export {
   createUser,
-  userLogin,
-  getCenters
 };

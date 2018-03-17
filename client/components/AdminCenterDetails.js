dbe39280c3c2a67dffb6a9a6434283b483ad5c8f
Olusola Oseni <kenolusola@gmail.com>
2018-03-17 11:41:09 +0100

    Create container component to handle center details

---
+++ client/components/AdminCenterDetails.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import {link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+import PropTypes from 'prop-types';
+import * as centerActions from '.././actions/CenterActions'
+import { eventCenters } from '.././reducers/';
+import AdminCenterDetail from './AdminCenterDetail';
+
+class AdminCenterDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      center: this.props.eventCenters.center
+    }
+  }
+  componentWillMount() {
+    let id = this.props.match.params.id;
+    this.props.getOneCenter(id);
+  }
+  
+  componentWillReceiveProps(nextProps){
+    if (nextProps.eventCenters.center !== this.props.eventCenters.center) {
+      this.setState({
+        center: nextProps.eventCenters.center
+      })
+    }
+  }
+  render () {
+    const { center } = this.state;
+    const { getOneCenter} = this.props;
+    const { id } = this.props.match.params;
+    
+    return (
+      <div>
+        {center !== {} && <AdminCenterDetail center={center} getOneCenter={getOneCenter} id={id} />}
+      </div>
+    )
+  }
+}
+AdminCenterDetails.propTypes = {
+  getOneCenter: PropTypes.func.isRequired,
+}
+const mapStateToProps = state => ({
+  eventCenters: state.eventCenters
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(centerActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(AdminCenterDetails)

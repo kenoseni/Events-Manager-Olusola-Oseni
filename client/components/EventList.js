71f7f843a4139000f4db70f90bec6ec046bbe3c3
Olusola Oseni <kenolusola@gmail.com>
2018-03-17 15:51:30 +0100

    Add component to pass event data

---
+++ client/components/EventList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Navbar3 from './Navbar3';
+import Event from './Event';
+
+class EventList extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { events} = this.props;
+    return (
+      <div style={{paddingTop: '60px'}}>
+        <Navbar3 />
+        {(events.length === 0) ? 
+        <h1 style={{marginTop:'150px', paddingLeft: '300px'}}>No Events Created, Add an Event</h1> :
+        events.map((event, i) => <Event {...this.props} key={event.id} i={i} event={event} />)
+        }
+      </div>
+    )
+  }
+}
+EventList.propTypes = {
+  events: PropTypes.array.isRequired,
+}
+
+export default EventList;

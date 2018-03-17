a928f734630e94ea8602a4afb3becd403aab3d44
Olusola Oseni <kenolusola@gmail.com>
2018-03-17 15:37:46 +0100

    Add reducer to handle getting all event actions

--- client/reducers/EventReducer.js
+++ client/reducers/EventReducer.js
@@ -1,34 +1,54 @@
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case 'ALL_USER_EVENT': {
-      return {
-        ...state,
-        status: {
-          ...state.status,
-          fetching: true,
-          fetched: false,
-          error: false
-        }
-      };
-    }
-    case 'ALL_USER_EVENT_RESOLVED': {
-      const { name, date, time, centerId } = action.payload;
-      const newEvent = { name, date, time, centerId };
-      console.log(action.payload);
-      return action.payload.events;
-    }
-    case 'ALL_USER_EVENT_REJECTED': {
-      return {
-        ...state,
-        status: {
-          ...state.status,
-          fetching: false,
-          error: action.payload
-        }
-      };
-    }
-    default: {
-      return state;
+/**
+* Class representing event reducer
+*
+* @class eventReducer
+*/
+class eventReducer {
+  /**
+  *
+  * @static
+  * @param {object} state - The initial state of the store
+  * @param {object} action - The action to be dispatched
+  * @return {object} new state of the store
+  * @memberof eventReducer
+  */
+  static userEvents(state = {
+    status: '',
+    message: '',
+    events: [],
+    event: {}
+  }, action) {
+    switch (action.type) {
+      case 'ALL_USER_EVENT': {
+        return {
+          ...state
+        };
+      }
+      case 'ALL_USER_EVENT_RESOLVED': {
+        const {
+          status, message, events
+        } = action.payload.data;
+        return {
+          ...state,
+          status,
+          message,
+          events
+        };
+      }
+      case 'ALL_USER_EVENT_REJECTED': {
+        const {
+          status, message
+        } = action.payload;
+        return {
+          ...state,
+          status,
+          message
+        };
+      }
+      default: {
+        return state;
+      }
     }
   }
 }
+export default eventReducer;

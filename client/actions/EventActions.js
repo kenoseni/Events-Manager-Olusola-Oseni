ead14c6899a298ba9060a79b6b3245e3d8e18670
Olusola Oseni <kenolusola@gmail.com>
2018-03-17 15:34:33 +0100

    Add action creator to get all event

--- client/actions/EventActions.js
+++ client/actions/EventActions.js
@@ -1,15 +1,25 @@
 import axios from 'axios';
-// import history from 'history';
-export const getUserEvents = () => (dispatch) => {
+
+const allUserEvents = () => (dispatch) => {
+  dispatch({
+    type: 'ALL_USER_EVENT',
+  });
   axios({
     method: 'get',
-    url: `http://localhost:${process.env.PORT}/api/v1/events`,
+    url: '/api/v1/events',
     headers: { 'x-access-token': localStorage.getItem('x-access-token') }
   })
     .then((res) => {
       dispatch({ type: 'ALL_USER_EVENT_RESOLVED', payload: res.data });
     })
     .catch((err) => {
-      dispatch({ type: 'ALL_USER_EVENT_REJECTED', payload: err.response.data });
+      dispatch({
+        type: 'ALL_USER_EVENT_REJECTED',
+        payload: err.response.data.data
+      });
     });
+};
+
+export {
+  allUserEvents,
 };

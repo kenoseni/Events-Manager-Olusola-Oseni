fbe688410ee4dc8d0a7dac6f5afb18c6c3fe86fd
Olusola Oseni <kenolusola@gmail.com>
2018-03-17 01:46:52 +0100

    Add reducers to handle get all centers

--- client/reducers/CenterReducer.js
+++ client/reducers/CenterReducer.js
@@ -1,34 +1,54 @@
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case 'ALL_CENTERS': {
-      return {
-        ...state,
-        status: {
-          ...state.status,
-          fetching: true,
-          fetched: false,
-          error: false
-        }
-      };
-    }
-    case 'ALL_CENTERS_RESOLVED': {
-      const { name, description, location, id, address, avaliability } = action.payload;
-      const newCenter = { name, description, location, id, address, avaliability };
-      console.log(action.payload)
-      return action.payload.center
-    }
-    case 'ALL_CENTERS_REJECTED': {
-      return {
-        ...state,
-        status: {
-          ...state.status,
-          fetching: false,
-          error: action.payload
-        }
-      };
-    }
-    default: {
-      return state;
+/**
+* Class representing center reducer
+*
+* @class centerReducer
+*/
+class centerReducer {
+  /**
+  *
+  * @static
+  * @param {object} state - The initial state of the store
+  * @param {object} action - The action to be dispatched
+  * @return {object} new state of the store
+  * @memberof centerReducer
+  */
+  static eventCenters(state = {
+    status: '',
+    message: '',
+    centers: [],
+    center: {}
+  }, action) {
+    switch (action.type) {
+      case 'ALL_CENTERS': {
+        return {
+          ...state
+        };
+      }
+      case 'ALL_CENTERS_RESOLVED': {
+        const {
+          status, message, centers
+        } = action.payload;
+        return {
+          ...state,
+          status,
+          message,
+          centers
+        };
+      }
+      case 'ALL_CENTERS_REJECTED': {
+        const {
+          status, message
+        } = action.payload;
+        return {
+          ...state,
+          status,
+          message
+        };
+      }
+      default: {
+        return state;
+      }
     }
   }
 }
+export default centerReducer;

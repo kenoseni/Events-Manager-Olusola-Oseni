dd7032998a018f3fe501665c5adc5adec1916f32
Olusola Oseni <kenolusola@gmail.com>
2018-03-26 04:10:34 +0100

    Add component to handle signout

---
+++ client/components/SignOut.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {signout} from '../actions/UserActions';
+
+class SignOut extends Component {
+
+  componentWillMount() {
+    this.props.signout();   
+  };
+
+  render() {
+    return <Redirect to="/" push />;
+  }
+}
+
+SignOut.propTypes = {
+  signout: PropTypes.func.isRequired
+}
+export default connect((state) => {return {}}, {signout} )(SignOut)

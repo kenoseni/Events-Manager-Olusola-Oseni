483de5c31b7f385e664ced317e9d924f7abf135f
Olusola Oseni <kenolusola@gmail.com>
2018-04-10 05:33:17 +0100

    renders the admin view

---
+++ client/components/AdminPanelList.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import AdminPanel from './AdminPanel';
+import NavBar from './NavBar';
+
+class AdminPanelList extends Component {
+  render () {
+    const { users, match } = this.props;
+    return (
+      <div style={{paddingTop: '90px'}}>
+        <NavBar home='Home' admin='Admin' centers='Centers' events='Events' page='Admin' logout='Log Out' match={match} />
+        <div className="container">
+          <div className="row" id="users">
+            <AdminPanel users={users} match={match} />  
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+AdminPanelList.propTypes = {
+  users: PropTypes.array,
+  match: PropTypes.object.isRequired
+}
+export default AdminPanelList

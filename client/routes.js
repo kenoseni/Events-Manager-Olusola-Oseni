bfa590c0b5613193c0c7844f6c5507b59c05b01f
Olusola Oseni <kenolusola@gmail.com>
2018-04-10 05:22:28 +0100

    Add admin route

--- client/routes.js
+++ client/routes.js
@@ -4,14 +4,12 @@ import { createBrowserHistory } from 'history';
 
 import LandingPage from './components/LandingPage';
 import AdminCenters from './components/AdminCenters';
+import AdminPanels from './components/AdminPanels';
 import SignOut from './components/SignOut';
 import AdminCenterDetails from './components/AdminCenterDetails';
 import Events from './components/Events';
 import NotFound from './components/NotFound';
 import checkAuth from './utils/checkAuthentication';
-
-
-
 
 export const history = createBrowserHistory();
 
@@ -22,6 +20,7 @@ export const Routes = () => {
       <Route exact path='/centers' component={checkAuth(AdminCenters)}/>
       <Route exact path='/centers/:id' component={checkAuth(AdminCenterDetails)} />
       <Route exact path='/events' component={checkAuth(Events)}/>
+      <Route exact path='/admin' component={checkAuth(AdminPanels)}/>
       <Route exact path='/logout' component={SignOut}/>
       <Route exact path='*' component={NotFound}/>
     </Switch>

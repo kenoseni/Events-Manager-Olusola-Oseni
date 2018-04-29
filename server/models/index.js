8ba6e0ee4d05abd9b79a9c9f9d72dd0962e17e47
Olusola Oseni <andeladeveloper@Andelas-MacBook-Pro-64.local>
2018-04-29 19:05:50 +0100

    Change directory of server config

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import Sequelize from 'sequelize';
-import config from '../config/config';
+import config from '../../lib/config/config';
 
 const sequelize = new Sequelize(config.url, config);
 const database = {};
